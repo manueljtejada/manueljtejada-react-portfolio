f6872b0775529855cf55abf5b43a117c7d14daf8
Manuel Tejada <manute32@gmail.com>
2017-09-14 15:37:15 -0400

    Add contentful client

---
+++ src/services/client.js
@@ -0,0 +1,7 @@
+import { createClient } from 'contentful';
+import { deliveryAccessToken, spaceId } from '../config';
+
+export const client = createClient({
+  space: spaceId,
+  accessToken: deliveryAccessToken
+});
