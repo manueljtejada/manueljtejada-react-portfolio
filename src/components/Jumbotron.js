510c460be72db6cc73d8fd0d48523eb462e1b672
Manuel Tejada <manute32@gmail.com>
2017-10-04 22:18:39 -0400

    Install `styled-components`

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import image from '../images/jumbotron.jpg';
 
 import TypeAnimation from './TypeAnimation';
 
+const Wrapper = styled.header`
+	background: url(${image}) no-repeat left top;
+	background-size: cover;
+	color: #fff;
+	height: 450px;
+	margin: -24px -15px 0;
+	padding-top: 120px;
+	text-align: center;
+	z-index: -9;
+	h1 {
+		font-weight: 700;
+	}
+`;
+
 const Jumbotron = () => {
 	return (
-		<div className="jumbotron jumbotron-fluid">
+		<Wrapper>
 			<div className="container">
 				<h1>Hello, I'm Manuel.</h1>
 				<p className="lead">
@@ -20,7 +37,7 @@ const Jumbotron = () => {
 			<a className="arrow-down">
 				<i className="fa fa-long-arrow-down fa-2x" />
 			</a>
-		</div>
+		</Wrapper>
 	);
 };
 
